5e0b094ba620fa8fbe81143707a581fa91334e13
Luís Farias <fluisfm20@gmail.com>
2022-04-25 15:07:28 -0300

    fix: Fix typo breakfast

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Brakfast menu')
+streamlit.header('Breakfast menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinake & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
