96a3f91741a3daddd37399b627da26147609e206
Luís Farias <fluisfm20@gmail.com>
2022-04-27 16:02:45 -0300

    feat: Query Our Trial Account Metadata

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,10 @@ fruitvyce_response = requests.get('https://fruityvice.com/api/fruit/'+
 fruitvyce_normalized = pd.json_normalize(fruitvyce_response.json())
 #output it the screen as a table
 streamlit.dataframe(fruitvyce_normalized)
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
